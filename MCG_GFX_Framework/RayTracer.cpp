181e9c901296e85a325e0a2af49bf49f1cca1efb
Cyan244466666 <44552719+Cyan244466666@users.noreply.github.com>
2021-04-12 16:22:14 +0100

    Update #1

---
+++ MCG_GFX_Framework/RayTracer.cpp
@@ -0,0 +1,29 @@
+#include "RayTracer.h"
+
+Light directionalLight;
+
+// Constructor
+RayTracer::RayTracer() {}
+// Destructor
+RayTracer::~RayTracer() {}
+
+
+glm::vec3 RayTracer::GenerateColor(Ray ray, Sphere sphere)
+{
+	// Find out whether the ray has intersected with the sphere:
+	hitInformation hitInfo = sphere.IsIntersection(ray);
+	
+	// If the ray hit the sphere, calculate sphere colour:
+	glm::vec3 newColor = sphere.GetColor();
+
+	if (hitInfo.hasHit)
+	{
+		// Create the colour modifier for directional light:
+		float coeff = glm::dot(directionalLight.lightDirection, hitInfo.hitNormal);
+		newColor *= coeff;
+		return newColor;
+	}
+	// If the ray didn't hit the sphere, return the background colour:
+	newColor = { 0.f, 0.f, 0.f };
+	return newColor;
+}
